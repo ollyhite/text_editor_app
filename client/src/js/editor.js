160f44d5e2842d15dd461dc6dcc40965b5f1f9ad
PeiYun <ollylee8520@gmail.com>
2022-07-04 20:45:36 -0600

    fixed show header text and save to the db n localstorage/Set the cursor at the end/link favicon/icon in header/fixed the error in inspect

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -5,6 +5,7 @@ import { header } from './header';
 export default class {
   constructor() {
     const localData = localStorage.getItem('content');
+    console.log("default localData",localData);
 
     // check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
@@ -21,21 +22,36 @@ export default class {
       indentUnit: 2,
       tabSize: 2,
     });
-
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
+      console.log("begining data",data);
+      console.log("begining localData",localData);
+      if(!data || !localData){
+        this.editor.setValue(header);
+      }
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      // this.editor.setValue( data || localData || header);
+      // this.editor.setValue( header || data || localData);
+      // this.editor.setValue( localData || data[0].content || header);
+      this.editor.setValue( localData?data[0].content:header);
+
+      // Set the cursor at the end function
+      this.editor.focus();
+      // Set the cursor at the end of existing content
+      this.editor.setCursor(this.editor.lineCount(), 0);
     });
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
+      // console.log("new data --change",localStorage.getItem('content'));
+      putDb(localStorage.getItem('content'));
     });
 
     // Save the content of the editor when the editor itself is loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
+      // console.log("new data --blur",localStorage.getItem('content'));
       putDb(localStorage.getItem('content'));
     });
   }
